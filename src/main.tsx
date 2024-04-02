f866c49f8f9f17fa3265757a28b1a8ae68c50fbd
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-04-02 23:17:38 +0000

    Added router

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LaunchPage from "./pages/launch.tsx";
+
 
 const apollo = new ApolloClient({
   uri: "https://spacex-production.up.railway.app",
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/launch/:launchId",
+    element: <LaunchPage />,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={apollo}>
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>,
 );
